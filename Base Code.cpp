10e82ce59acd35500fe96f8a12b0e4536c486996
/-\ - Pedro Henrique Klein <pedrohenriquekleinphg@gmail.com>
2022-03-09 19:09:33 -0300

    add Base Code

---
+++ Base Code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <new>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+#include <ctime>
+#include <numeric>  
+#include <functional>
+
+using namespace std;
+
+
+int main() {
+
+
+    return 0;
+}
